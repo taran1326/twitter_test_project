9fd435eb1615c841e5b277baac5e28c8b950e202
Taran Mittal <taranmittal.1717@gmail.com>
2023-03-06 16:24:26 +0530

    automated the database process

--- Controllers/tweet.test.js
+++ Controllers/tweet.test.js
@@ -1,7 +1,12 @@
 const request = require('supertest');
 const app = require('../index');
 const db = require('../db');
-const { isAuth } = require('../Utils/Auth');
+const {connect} = require('../db');
+const tb_tweet = require('../Schemas/Tweets');
+const tb_user = require('../Schemas/User');
+
+// const {TwitterDB} = require('../db').TwitterDB;
+// const { isAuth } = require('../Utils/Auth');
 // jest.setTimeout(40000);
 
 //tweet check same as sign up just check the ß_id property response
@@ -93,7 +98,7 @@ describe('Check if username has a correct format' , ()=>{
                                         .send(data);
         console.log(JSON.stringify(response.body));
         expect(response.body.status).toBe(401 );
-        expect(response.body).toHaveProperty('messsssage' );
+        expect(response.body).toHaveProperty('message' );
 
     });
 
@@ -215,167 +220,183 @@ describe('Check if password has a correct format' , ()=>{
 
 
 
-let res , res1 ; 
-describe('User X Sign Up functionality check' , ()=> {
-       
-    test('User X should be able to register and login if credentials are valid' , async()=>{
-        const data = {
-            "name": "John",
-            "username": "john1353",
-            "email": "john3152@mail.com",
-            "password": "John1234"
-        }
+describe('Database test suite' , () => {
+
+    beforeAll( async () => {
+        await connect();
+    })
 
 
-        JSON.stringify(data);
-        res = await request(app).post('/auth/register')
-                                .set('Content-type', 'application/json')
-                                .send(data)
-
-
-        expect(res.statusCode).toBe(200);
-        expect(res.body.data).toHaveProperty('username');
-        expect(res.body.data).toHaveProperty('name');
-        expect(res.body.data).toHaveProperty('email');
+    beforeAll(async () => {
+        await tb_tweet.deleteMany({});
         
-    });
-
-});
-
-describe('User X Sign In functionality check ', ()=>{
-    it('User X should be able to sign in with valid credentials' , async()=>{
-        res1 = await request(app).post('/auth/login')
-                                 .set('Content-type', 'application/json')
-                                 .send({
-                                        'loginId':"john1353",
-                                        'password':'John1234'
-                                 }).expect(200);
-        expect(res1.body).toHaveProperty('message');
-        expect(res1.body.data).toHaveProperty('email');
-        expect(res1.body.data).toHaveProperty('_id');
-        expect(res1.body.data).toHaveProperty('username');
+        // await mongoose.connection.close();
     })
-}); 
 
-
-
-
-
-let response;
-describe('User X Create Tweeet functionality' , ()=>{
-    test('user X should be able to create a new tweet' , async() =>{
-        const creationDatetime = new Date();
-        console.log(console.dir(res1.body));
-        const data1 = {
-            'title':'Hello world!', 
-            'bodyText':'Helloworld i am tester',
-            'userId':res1.body.data._id,
-            'creationDatetime':creationDatetime
-
-        }
-        JSON.stringify(data1);
-
-        response = await request(app).post('/tweet/create-tweet')
-                                        //    .set(isAuth , true)
-                                            .set('Cookie' , res1.headers['set-cookie'] )
-                                            .send(data1);
+    beforeAll(async () =>{
+        await tb_user.deleteMany({});
+    })
+    let res , res1 ; 
+    describe('User X Sign Up functionality check' , ()=> {
         
-        console.log(console.dir(response.body));
+        test('User X should be able to register and login if credentials are valid' , async()=>{
+            const data = {
+                "name": "John",
+                "username": "john1353",
+                "email": "john3152@mail.com",
+                "password": "John1234"
+            }
 
-        expect(response.statusCode).toBe(200);
-        expect(response.body.data).toHaveProperty('userId');
-    
+
+            JSON.stringify(data);
+            res = await request(app).post('/auth/register')
+                                    .set('Content-type', 'application/json')
+                                    .send(data)
+
+
+            expect(res.statusCode).toBe(200);
+            expect(res.body.data).toHaveProperty('username');
+            expect(res.body.data).toHaveProperty('name');
+            expect(res.body.data).toHaveProperty('email');
+            
+        });
+
     });
-})
 
-let res7;
-describe('User X and User Y Logout functionality' , ()=>{
-    it('User should be able to logout successfully' , async()=>{
-        res7 = await request(app).post('/auth/logout')
-                           .set('Cookie' , res1.headers['set-cookie'])
-                           .expect(200);
-        expect(res7.body).toHaveProperty('message');
-        expect(res7.body.data).toHaveProperty('email');
-        // expect(res7.body.data).toHaveProperty('message');
+    describe('User X Sign In functionality check ', ()=>{
+        it('User X should be able to sign in with valid credentials' , async()=>{
+            res1 = await request(app).post('/auth/login')
+                                    .set('Content-type', 'application/json')
+                                    .send({
+                                            'loginId':"john1353",
+                                            'password':'John1234'
+                                    }).expect(200);
+            expect(res1.body).toHaveProperty('message');
+            expect(res1.body.data).toHaveProperty('email');
+            expect(res1.body.data).toHaveProperty('_id');
+            expect(res1.body.data).toHaveProperty('username');
+        })
+    }); 
 
-    })
-})
 
-let res2 , res3 , res4;
-describe('User Y should be able to sign up , sign in and create a tweet with valid credentials' , ()=>{
-    it('User Y should be able to sign up' , async() =>{
-        const data2 = {
-            "name": "Taran",
-            "username": "taran1326",
-            "email": "taran1326@mail.com",
-            "password": "Taran1326"
-        }
+
+
+
+    let response;
+    describe('User X Create Tweeet functionality' , ()=>{
+        test('user X should be able to create a new tweet' , async() =>{
+            const creationDatetime = new Date();
+            console.log(console.dir(res1.body));
+            const data1 = {
+                'title':'Hello world!', 
+                'bodyText':'Helloworld i am tester',
+                'userId':res1.body.data._id,
+                'creationDatetime':creationDatetime
+
+            }
+            JSON.stringify(data1);
+
+            response = await request(app).post('/tweet/create-tweet')
+                                            //    .set(isAuth , true)
+                                                .set('Cookie' , res1.headers['set-cookie'] )
+                                                .send(data1);
+            
+            console.log(console.dir(response.body));
+
+            expect(response.statusCode).toBe(200);
+            expect(response.body.data).toHaveProperty('userId');
         
-        JSON.stringify(data2);
-        res2 = await request(app).post('/auth/register').set('Content-type', 'application/json')
-                                             .send(data2);
+        });
+    })
+
+    let res7;
+    describe('User X and User Y Logout functionality' , ()=>{
+        it('User should be able to logout successfully' , async()=>{
+            res7 = await request(app).post('/auth/logout')
+                            .set('Cookie' , res1.headers['set-cookie'])
+                            .expect(200);
+            expect(res7.body).toHaveProperty('message');
+            expect(res7.body.data).toHaveProperty('email');
+            // expect(res7.body.data).toHaveProperty('message');
+
+        })
+    })
+
+    let res2 , res3 , res4;
+    describe('User Y should be able to sign up , sign in and create a tweet with valid credentials' , ()=>{
+        it('User Y should be able to sign up' , async() =>{
+            const data2 = {
+                "name": "Taran",
+                "username": "taran1326",
+                "email": "taran1326@mail.com",
+                "password": "Taran1326"
+            }
+            
+            JSON.stringify(data2);
+            res2 = await request(app).post('/auth/register').set('Content-type', 'application/json')
+                                                .send(data2);
+        });
+
+        it('User Y should be able to sign in', async()=>{
+            res3 = await request(app).post('/auth/login')
+                                    .set('Content-type', 'application/json')
+                                    .send({
+                                            'loginId':"taran1326",
+                                            'password':'Taran1326'
+                                        });
+        });
+
+        it('User Y should be able to create a tweet successfully' , async()=>{
+            const creationDatetime1 = new Date();
+            const data3 = {
+                'title':'Hello world!', 
+                'bodyText':'Helloworld i am tester',
+                'userId':res3.body.data._id,
+                'creationDatetime':creationDatetime1
+
+            }
+            JSON.stringify(data3);
+            res4 = await request(app).post('/tweet/create-tweet')
+                                            //    .set(isAuth , true)
+                                                .set('Cookie' , res3.headers['set-cookie'] )
+                                                .send(data3);
+            // console.log(JSON.stringify(response.body));
+            console.log(console.dir(res4.body));
+            expect(res4.statusCode).toBe(200);
+            expect(res4.body.data).toHaveProperty('userId');
+        })
     });
 
-    it('User Y should be able to sign in', async()=>{
-        res3 = await request(app).post('/auth/login')
-                                 .set('Content-type', 'application/json')
-                                 .send({
-                                        'loginId':"taran1326",
-                                        'password':'Taran1326'
-                                    });
+    let res5;
+    describe('User Y should be able to see feed of all the tweeters' , ()=>  {
+        it('should get all tweets and should be stored in chronological order' , async()=>{
+
+            res5 = await request(app).get('/tweet/feed')
+                        .set('Cookie' , res3.headers['set-cookie'])
+                        .set('Content-type' , 'application/json')
+                        .expect(200)
+
+            let ans = (res5.body.data[0].creationDatetime) > (res5.body.data[1].creationDatetime)
+            expect(ans).toBe(true);
+            expect(res5.body.data.length).toBe(2);
+            console.log(res5.body.data.length);
+                        
+        })
     });
 
-    it('User Y should be able to create a tweet successfully' , async()=>{
-        const creationDatetime1 = new Date();
-        const data3 = {
-            'title':'Hello world!', 
-            'bodyText':'Helloworld i am tester',
-            'userId':res3.body.data._id,
-            'creationDatetime':creationDatetime1
 
-        }
-        JSON.stringify(data3);
-        res4 = await request(app).post('/tweet/create-tweet')
-                                        //    .set(isAuth , true)
-                                            .set('Cookie' , res3.headers['set-cookie'] )
-                                            .send(data3);
-        // console.log(JSON.stringify(response.body));
-        console.log(console.dir(res4.body));
-        expect(res4.statusCode).toBe(200);
-        expect(res4.body.data).toHaveProperty('userId');
-    })
+    let res6;
+    describe('User Y should able to see his/her feed ' , ()=> {
+        it('should be able to see my tweets' , async()=>{
+            const res6 = await request(app).get('/tweet/my-tweets')
+                                            .set('Content-type' , 'application/json')
+                                            .set('Cookie' , res3.headers['set-cookie'])
+                                            .expect(200)
+            expect(res6.body.data.length).toBe(1);
+            console.log(res6.body.data.length);
+        })
+    });
 });
-
-let res5;
-describe('User Y should be able to see feed of all the tweeters' , ()=>  {
-    it('should get all tweets and should be stored in chronological order' , async()=>{
-
-        res5 = await request(app).get('/tweet/feed')
-                    .set('Cookie' , res3.headers['set-cookie'])
-                    .set('Content-type' , 'application/json')
-                    .expect(200)
-
-        let ans = (res5.body.data[0].creationDatetime) > (res5.body.data[1].creationDatetime)
-        expect(ans).toBe(true);
-        expect(res5.body.data.length).toBe(2);
-        console.log(res5.body.data.length);
-                    
-    })
-});
-
-
-let res6;
-describe('User Y should able to see his/her feed ' , ()=> {
-    it('should be able to see my tweets' , async()=>{
-        const res6 = await request(app).get('/tweet/my-tweets')
-                                        .set('Content-type' , 'application/json')
-                                        .set('Cookie' , res3.headers['set-cookie'])
-                                        .expect(200)
-        expect(res6.body.data.length).toBe(1);
-        console.log(res6.body.data.length);
-    })
-});
-
 
 
         
--- db.js
+++ db.js
@@ -1,15 +1,37 @@
 const mongoose = require('mongoose');
 const app = require('./index');
 
-mongoose.connect("mongodb://0.0.0.0:27017/TwitterDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected with mongodb');
-    const PORT = 3000;
-    app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`);
-    })
-}).catch(err => {
-    console.log('Error in connecting with mongo');
-})
+
+async function connect(){
+    try{
+        await mongoose.connect("mongodb://0.0.0.0:27017/TwitterDB" , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to database");
+        const PORT = 3000;
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+        });
+    }catch(err){
+        console.log('Error in connecting with mongo');
+    }
+}
+
+
+module.exports = {connect};
+// mongoose.connect("mongodb://0.0.0.0:27017/TwitterDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log('Connected with mongodb');
+//     const PORT = 3000;
+//     app.listen(PORT, () => {
+//         console.log(`Listening on port ${PORT}`);
+//     })
+// }).catch(err => {
+//     console.log('Error in connecting with mongo');
+// })
+
+
+
